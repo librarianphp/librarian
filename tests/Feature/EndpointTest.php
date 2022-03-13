cc9ee72a7df3a8ff17b75db91f6e24dd5f21717c
Erika Heidi <erika@erikaheidi.com>
2022-03-13 10:54:08 +0100

    Skipping endpoint test when on GH Actions

--- tests/Feature/EndpointTest.php
+++ tests/Feature/EndpointTest.php
@@ -3,9 +3,9 @@
 it('serves the index page', function () {
     $response = getResponseCode('/');
     expect($response)->toBe(200);
-});
+})->skip(getenv('GITHUB_ACTIONS'), 'Running on GH...');
 
 it('redirects to 404 page when content is not found', function () {
     $response = getResponseCode('/lskdsajdlajdlkad');
     expect($response)->toBe(303);
-});
+})->skip(getenv('GITHUB_ACTIONS'), 'Running on GH...');;
