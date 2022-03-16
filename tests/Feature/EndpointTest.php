5ff5c585ddb0a0b0d737d4a179412d9bcffd2626
Erika Heidi <erika@erikaheidi.com>
2022-03-16 16:50:19 +0100

    Updated librarian-core dependency to stable release, added tests

--- tests/Feature/EndpointTest.php
+++ tests/Feature/EndpointTest.php
@@ -15,3 +15,11 @@ it('redirects to 404 page when content is not found', function () {
 it('serves the default about page', function () {
     $this->matchResponse($this->base_url . '/_p/about', 200);
 })->skip(getenv('GITHUB_ACTIONS'), 'Running on GH...');
+
+it('serves the feed', function () {
+    $this->matchResponse($this->base_url . '/feed', 200);
+})->skip(getenv('GITHUB_ACTIONS'), 'Running on GH...');
+
+it('shows not found message in 404 page', function () {
+    $this->responseContains($this->base_url . '/notfound', '404');
+})->skip(getenv('GITHUB_ACTIONS'), 'Running on GH...');
