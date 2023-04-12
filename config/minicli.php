50c6a91f1b268d146c55f801245c3cb2d89a9152
Erika Heidi <erika@erikaheidi.com>
2023-04-12 20:42:17 +0200

    moved help and create commands

--- config/minicli.php
+++ config/minicli.php
@@ -6,7 +6,11 @@ return [
      * You shouldn't need to change the next settings,
      * but you are free to do so at your own risk.
      *****************************************************************************/
-    'app_path' => __DIR__ . '/../app/Command',
+    'app_path' => [
+        __DIR__ . '/../app/Command',
+        '@librarianphp/command-help',
+        '@librarianphp/command-create',
+    ],
     'theme' => 'unicorn',
     'templates_path' => __DIR__ . '/../app/Resources/themes/default',
     'data_path' => __DIR__ . '/../app/Resources/data',
