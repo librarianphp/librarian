0a341384f2787576ac50ce5f52281b2c8a4add26
Erika Heidi <erika@erikaheidi.com>
2023-04-23 17:27:32 +0200

    Decomissioning devtoserviceprovider

--- web/index.php
+++ web/index.php
@@ -7,7 +7,6 @@ use Librarian\Provider\TwigServiceProvider;
 use Librarian\Provider\RouterServiceProvider;
 use Librarian\Exception\RouteNotFoundException;
 use Librarian\Provider\ContentServiceProvider;
-use Librarian\Provider\DevtoServiceProvider;
 use Librarian\Provider\LibrarianServiceProvider;
 use Librarian\Response;
 
@@ -17,7 +16,6 @@ $app->addService('twig', new TwigServiceProvider());
 $app->addService('router', new RouterServiceProvider());
 $app->addService('content', new ContentServiceProvider());
 $app->addService('librarian', new LibrarianServiceProvider());
-$app->addService('devto', new DevtoServiceProvider());
 
 $app->librarian->boot();
 
