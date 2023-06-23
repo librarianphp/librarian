2b03f2faa656dd12ce2f6bedc5e16d4e8c951041
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-06-23 12:52:49 +0100

    Start the fix for the minicli update

--- tests/Pest.php
+++ tests/Pest.php
@@ -39,39 +39,12 @@ expect()->extend('toBeOne', fn () => $this->toBe(1));
 |
 */
 
-use Librarian\Provider\ContentServiceProvider;
-use Librarian\Provider\LibrarianServiceProvider;
-use Librarian\Provider\RouterServiceProvider;
-use Librarian\Provider\TwigServiceProvider;
 use Minicli\App;
 use Minicli\Command\CommandCall;
 
-function getCommandsPath(): string
-{
-    return __DIR__ . '/../app/Command';
-}
-
 function getApp(): App
 {
-    return new App(load_config(__DIR__ . '/../config'));
-}
-
-function getWebApp(): App
-{
-    $app = getApp();
-    $app->addService('content', new ContentServiceProvider());
-    $app->addService('twig', new TwigServiceProvider());
-    $app->addService('librarian', new LibrarianServiceProvider());
-    $app->addService('router', new RouterServiceProvider());
-
-    return $app;
-}
-
-function getConfigValue(string $key): mixed
-{
-    $config = load_config();
-
-    return $config[$key] ?? null;
+    return new App([], 'Type "./librarian help" for help with available commands.', __DIR__);
 }
 
 function getCommandCall(?array $parameters = null): CommandCall
